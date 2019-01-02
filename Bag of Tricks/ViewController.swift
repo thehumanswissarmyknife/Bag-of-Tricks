7e49717e18fbdbc4004f4660e99069393bbeab42
Dennis <dennis@thehumanswissarmyknife.de>
2019-01-02 15:33:28 +0100

    Initial Commit

---
+++ Bag of Tricks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bag of Tricks
+//
+//  Created by Dennis Vocke on 02.01.19.
+//  Copyright © 2019 Dennis Vocke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
