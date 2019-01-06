1020daaa1ea765069688ced21f31928013584bd9
Dennis <dennis@thehumanswissarmyknife.de>
2019-01-06 11:30:14 +0100

    delete class trick() and class bagOfTricks()

--- Bag of Tricks/controller/MainViewController.swift
+++ Bag of Tricks/controller/MainViewController.swift
@@ -19,17 +19,17 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let player1 = Player(thisName: "Frank", thisLevel: "easy", thisID : 11)
-        let player2 = Player(thisName: "Peter", thisLevel: "easy", thisID : 21)
-        let player3 = Player(thisName: "Louise", thisLevel: "easy", thisID : 31)
-        
-        let myPlayers = [player1, player2, player3]
-        print("new MainViewController")
-        
-        let myBagOfTricks = BagOfTricks(theseManyTricks : 5, thisTrump : "green", thesePlayers : myPlayers)
-        
-        
-        myBagOfTricks.playBag()
+//        let player1 = Player(thisName: "Frank", thisLevel: "easy", thisID : 11)
+//        let player2 = Player(thisName: "Peter", thisLevel: "easy", thisID : 21)
+//        let player3 = Player(thisName: "Louise", thisLevel: "easy", thisID : 31)
+//        
+//        let myPlayers = [player1, player2, player3]
+//        print("new MainViewController")
+//        
+//        let myBagOfTricks = BagOfTricks(theseManyTricks : 5, thisTrump : "green", thesePlayers : myPlayers)
+//        
+//        
+//        myBagOfTricks.playBag()
     }
     
 
